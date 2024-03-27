c8e92af1f6e76b3b0980061856a21c2efb444ed7
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-03-27 19:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort((a,b)=>a-b);
+	let total = 0;
+	while(arr.length>1){
+		let a1= arr.shift();
+		let a2= arr.shift();
+		const merge= a1+a2;
+		total += merge;
+		arr.push(merge);
+		arr.sort((a,b)=>a-b);
+	}
+	return total;
 // return the min cost
   
 }
